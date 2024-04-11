9012d3a6160ff7948a78f7266861df1d28f5e3d1
zachuri <punsalang.zachary@gmail.com>
2024-04-11 00:09:08 -0700

    setup drizzle orm

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import { Hono } from "hono";
 
-const app = new Hono();
+export type Env = {
+	DATABASE_URL: string;
+	MY_VAR: string;
+};
+
+const app = new Hono<{ Bindings: Env }>();
 
 app.get("/", c => {
 	return c.text("Hello Hono!");
 });
 
 app.get("/customers", c => {
-	return c.json([{ id: 1, name: "hi" }]);
+	const secret = c.env.MY_VAR;
+	return c.json([{ id: 1, name: secret }]);
 });
 
 app.get("/customers/:id", c => {
