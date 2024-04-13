a8ab8edc845b2831fc76c6d83c59b80b9e5db880
zachuri <punsalang.zachary@gmail.com>
2024-04-13 02:03:16 -0700

    db client added schema to work with query method

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -1,10 +1,44 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle, NeonHttpDatabase } from "drizzle-orm/neon-http";
-import { Config } from "..";
+import { Config } from '..';
+import * as schema from '@/config/db/schema'
+import { BuildQueryResult, DBQueryConfig, ExtractTablesWithRelations } from 'drizzle-orm';
 
 export const getDBClient = (
 	databaseConfig: Config["database"]
-): NeonHttpDatabase => {
+)  => {
 	const client = neon(databaseConfig.url);
-	return drizzle(client);
+	return drizzle(client, {schema});
 };
+
+type Schema = typeof schema;
+type TablesWithRelations = ExtractTablesWithRelations<Schema>;
+
+export type IncludeRelation<TableName extends keyof TablesWithRelations> =
+  DBQueryConfig<
+    "one" | "many",
+    boolean,
+    TablesWithRelations,
+    TablesWithRelations[TableName]
+  >["with"];
+
+export type IncludeColumns<TableName extends keyof TablesWithRelations> =
+  DBQueryConfig<
+    "one" | "many",
+    boolean,
+    TablesWithRelations,
+    TablesWithRelations[TableName]
+  >["columns"];
+
+export type InferQueryModel<
+  TableName extends keyof TablesWithRelations,
+  Columns extends IncludeColumns<TableName> | undefined = undefined,
+  With extends IncludeRelation<TableName> | undefined = undefined,
+> = BuildQueryResult<
+  TablesWithRelations,
+  TablesWithRelations[TableName],
+  {
+    columns: Columns;
+    with: With;
+  }
+>;
--- src/config/db/schema.ts
+++ src/config/db/schema.ts
@@ -1,1 +1,2 @@
+export * from "@/config/db/table/users";
 export * from "@/models/product.model";
