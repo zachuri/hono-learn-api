7792d050fc54488df032bc0a8ace55c481ba3fd8
zachuri <punsalang.zachary@gmail.com>
2024-06-14 15:19:45 -0700

    added a logger

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,14 @@ import { ApiError } from "@/utils/ApiError";
 import { sentry } from "@hono/sentry";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { logger } from "hono/logger";
 import httpStatus from "http-status";
 
 const app = new Hono<Environment>();
 
 app.use("*", sentry());
 app.use("*", cors());
+app.use("*", logger());
 
 app.notFound(() => {
 	throw new ApiError(httpStatus.NOT_FOUND, "Not found");
