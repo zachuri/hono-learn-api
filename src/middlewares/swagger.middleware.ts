e9ed57f94a1617dec83768a660c703709ef75dfd
zachuri <punsalang.zachary@gmail.com>
2024-07-09 00:13:49 -0700

    feat: start of adding swagger api -> will add later

---
+++ src/middlewares/swagger.middleware.ts
@@ -0,0 +1,45 @@
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+
+const app = new OpenAPIHono();
+
+app.openapi(
+	createRoute({
+		method: "get",
+		path: "/hello",
+		responses: {
+			200: {
+				description: "Respond a message",
+				content: {
+					"application/json": {
+						schema: z.object({
+							message: z.string(),
+						}),
+					},
+				},
+			},
+		},
+	}),
+	c => {
+		return c.json({
+			message: "hello",
+		});
+	}
+);
+
+app.get(
+	"/ui",
+	swaggerUI({
+		url: "/doc",
+	})
+);
+
+app.doc("/doc", {
+	info: {
+		title: "An API",
+		version: "v1",
+	},
+	openapi: "3.1.0",
+});
+
+export default app;
